c5d014bb6441c5a0098799c758437fb0834c76ca
Anthony To <to.anthony93@gmail.com>
2015-03-27 22:14:38 -0500

    Configure mailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,25 @@
 Rails.application.configure do
   
-  config.action_mailer.default_url_options = { :host => 'zapskills.heroku.com' }
-  
+  ###### MAILER SETTINGS ######
+  # Ignore bad email addresses and do not raise email delivery errors.
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options   = { :host => 'zapskills.herokuapp.com' }
+  config.action_mailer.delivery_method       = :smtp
+  config.action_mailer.perform_deliveries    = true
+
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: ENV["GMAIL_DOMAIN"],
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+  }
+    
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
