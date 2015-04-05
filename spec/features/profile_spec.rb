54d13ea5c4889d02e29f41d21706b660f7b79a66
shachi agarwalla <shachiagarwalla@gmail.com>
2015-04-05 14:15:23 -0500

    More Profile feature tests
    Added few more profile tests:
    
    updated profile
    go to add skills page from edit profile
    edit user
    sign out from profile
    search page from profile

--- spec/features/profile_spec.rb
+++ spec/features/profile_spec.rb
@@ -3,32 +3,80 @@ require "rails_helper"
 RSpec.feature "the profile visiting and editing", :type => :feature do
   before :each do
     User.create(:email => "user@example.com", :password => "password")
-  end
-
-  scenario "visit profile page" do
     visit "/users/sign_in"
     within("#new_user") do
       fill_in "Email", :with => "user@example.com"
       fill_in "Password", :with => "password"
     end
     click_button "Log in"
-    expect(page).to have_content "Signed in successfully"
+  end
+
+  scenario "visit profile page" do
     click_link("Profile")
     expect(page).to have_content "Editing User"
   end
 
   scenario "update profile" do
-    visit "/users/sign_in"
-    within("#new_user") do
-      fill_in "Email", :with => "user@example.com"
-      fill_in "Password", :with => "password"
-    end
-    click_button "Log in"
     click_link("Profile")
-    #expect(page).to have_content "Update User"
     fill_in "First name", :with => "user"
     fill_in "Last name", :with => "example"
     click_button "Update User"
     expect(page).to have_content "User was successfully updated"
+  end
+  
+  scenario "updated profile" do
+    click_link("Profile")
+    fill_in "First name", :with => "user"
+    fill_in "Last name", :with => "example"
+    click_button "Update User"
+    expect(page).to have_content "First name: user"
+    expect(page).to have_content "Last name: example"
+  end
+
+  scenario "go to add skills page from edit profile" do
+    click_link("Profile")
+    click_button "Update User"
+    click_link("Add skills")
+    expect(page).to have_content "New Skill"
+  end
+
+#TODO
+  scenario "actually add skills" do
+    click_link("Profile")
+    click_button "Update User"
+    click_link("Add skills")
+    fill_in "Description", :with => "Learned it"
+    fill_in "experience_level", :with => "4"
+    select "2010", :from => "experience_start_date_1i"
+    select "November", :from => "experience_start_date_2i"
+    select "20", :from => "experience_start_date_3i"
+    #find('#experience_skill_id').find("option[value ='22']").click
+    #select "Cooking", :from => "experience_skill_id"
+    #click_button "Create Experience"
+  end
+
+  scenario "edit user" do
+    click_link("Profile")
+    click_button "Update User"
+    click_link("Edit")
+    expect(page).to have_content "Editing User"
+    fill_in "City", :with => "Madison"
+    fill_in "State", :with => "WI"
+    click_button "Update User"
+    expect(page).to have_content "User was successfully updated"
+    expect(page).to have_content "City: Madison"
+    expect(page).to have_content "State: WI"
+  end
+
+  scenario "sign out from profile" do
+    click_link("Profile")
+    click_link("Sign Out")
+    expect(page).to have_content "You need to sign in or sign up before continuing"
+  end
+
+  scenario "search page from profile" do
+    click_link("Profile")
+    click_link("Search")
+    expect(page).to have_content "Zapskills Home Search for skills:"
   end
 end
