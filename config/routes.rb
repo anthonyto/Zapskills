7e2c65fa3ae928273285cf32ffd91f8a86244f3b
Anthony To <to.anthony93@gmail.com>
2015-04-13 20:07:36 -0500

    Add erb to many of the routes, push footer to very bottom of page" "

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
   root 'welcome#main'
   
   get '/results', to: 'welcome#results', as: 'results'
+  get '/search', to: 'welcome#search', as: 'search'
+  get '/about', to: 'welcome#about', as: 'about'
+  get '/help', to: 'welcome#help', as: 'help'
+  get '/howto', to: 'welcome#howto', as: 'howto'
   get '/contact', to: 'welcome#contact', as: 'contact'
-  
+  get '/termsandconditions', to: 'welcome#termsandconditions', as: 'termsandconditions'
   
   resources :skills
 
