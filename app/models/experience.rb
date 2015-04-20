466ac60361c4adbd608d58d12d9dd05349f62716
Anthony To <to.anthony93@gmail.com>
2015-04-20 18:32:50 -0500

    Remove skill_id uniqueness validator on experience, because now no two users can have the same skill. gotta fix that

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,7 +2,7 @@ class Experience < ActiveRecord::Base
   belongs_to :user
   belongs_to :skill
   
-  validates :skill_id, uniqueness: true
+  # validates :skill_id, uniqueness: true
   validates_presence_of :skill_id, :level, :start_date, :description
   
 end
