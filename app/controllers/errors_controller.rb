c9e91ecab259683edb2aa983c39d31a40f8cd8f6
Anthony To <to.anthony93@gmail.com>
2015-04-19 21:12:21 -0500

    Authorize user edit page

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,4 @@
+class ErrorsController < ApplicationController 
+  def 401 
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+  
   def index
     @users = User.all
   end
 
+  # There needs to be authorization here about what user can see what profile and how those profiles look
   def show
+    @user = User.find(params[:id])
     if !current_user.complete_profile?
       render 'edit', notice: 'Please complete your profile.'
     end
@@ -16,6 +18,10 @@ class UsersController < ApplicationController
   end
 
   def edit
+    if !authorize_user(@user)
+      render 'errors/401'
+      return
+    end
   end
 
   def create
@@ -35,17 +41,23 @@ class UsersController < ApplicationController
     end
   end
 
+  # Do we need this?
   def destroy
     @user.destroy
     redirect_to users_url, notice: 'User was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
+  
+    def authorize_user(user)
+      user == current_user
     end
-
+    
+    # Use callbacks to share common setup or constraints between actions.
+     def set_user
+       @user = User.find(params[:id])
+     end
+   
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:first_name, :last_name, :email, :city, :state, :zip_code, :date_of_birth, :password, :profile_picture_url)
