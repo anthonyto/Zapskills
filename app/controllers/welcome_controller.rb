b350f9df45da3fc3ce0ae12b2f72d768f562a49c
Anthony To <to.anthony93@gmail.com>
2015-04-21 10:57:25 -0500

    Allow unauthorized access to main and footer pages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
+  skip_before_filter :authenticate_user!, only: [:main, :about, :contact, :help, :termsandconditions, :howto]
+  
   def search
     @skills = Skill.all
   end
