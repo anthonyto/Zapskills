388fbf8e36accfc4498678b38e453387c772e09d
Anthony To <to.anthony93@gmail.com>
2015-04-21 12:01:54 -0500

    Add rating to users and style results page a bit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420230456) do
+ActiveRecord::Schema.define(version: 20150421165347) do
 
   create_table "experiences", force: :cascade do |t|
     t.string   "description", limit: 255
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 20150420230456) do
     t.string   "avatar_content_type",    limit: 255
     t.integer  "avatar_file_size",       limit: 4
     t.datetime "avatar_updated_at"
+    t.float    "rating",                 limit: 53
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
