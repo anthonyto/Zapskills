43a8a57b17b1bdc4ec7d39af30f91c4d62006faa
shachi agarwalla <shachiagarwalla@gmail.com>
2015-04-25 00:18:11 -0500

    Refactor feature specs
    Refactor feature specs

---
+++ spec/features/signout_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.feature "Signout process: ", :type => :feature do
+  before :each do
+    User.create(:email => "user@example.com", :password => "password")
+  end
+
+  scenario "After signing in" do
+    visit "/users/sign_in"
+    within("#new_user") do
+      fill_in "Email", :with => "user@example.com"
+      fill_in "Password", :with => "password"
+    end
+    click_button "Log in"
+    expect(page).to have_content "Signed in successfully"
+    click_link("Sign Out")
+    expect(page).to have_content "Signed out successfully"
+  end
+
+  scenario "Without signing in redirects to signin page" do
+    visit "/users/sign_out"
+    expect(page).to have_content "You need to sign in or sign up before continuing"
+  end
+end
