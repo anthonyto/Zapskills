a9a9d420afaa8a9451f90ccf7f3f01e8205ac0b9
Anthony To <to.anthony93@gmail.com>
2015-04-26 15:49:02 -0500

    Implement around_me functionality with search function

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,13 +6,21 @@ class WelcomeController < ApplicationController
   end
   
   def results
-    begin
-      if search_params[:skill_id] == ""
-        @users = User.near("#{search_params[:city]}, #{search_params[:state]}", search_params[:radius]).where.not(id: current_user.id)
-      else
-        @users = Experience.find_by(skill: search_params[:skill_id]).skill.users.near("#{search_params[:city]}, #{search_params[:state]}", search_params[:radius]).where.not(id: current_user.id) 
+    if search_params[:around_me] == "1"
+      @users = User.near(current_user, search_params[:radius]).where.not(id: current_user.id)
+    else
+      begin
+        if search_params[:skill_id] == ""
+          @users = User.near("#{search_params[:city]}, #{search_params[:state]}", search_params[:radius]).where.not(id: current_user.id)
+        else
+          @users = Experience.find_by(skill: search_params[:skill_id]).skill.users.near("#{search_params[:city]}, #{search_params[:state]}", search_params[:radius]).where.not(id: current_user.id) 
+        end
+      rescue NoMethodError
+        @skills = Skill.all
+        redirect_to search_path, notice: "No results"
       end
-    rescue NoMethodError
+    end
+    if @users.empty?
       @skills = Skill.all
       redirect_to search_path, notice: "No results"
     end
@@ -40,7 +48,7 @@ class WelcomeController < ApplicationController
   private
   
   def search_params
-    params.permit(:skill_id, :city, :state, :radius)
+    params.permit(:skill_id, :city, :state, :radius, :around_me)
   end
   
 end
