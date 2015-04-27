a13100e49c6b64bda546ff406ffdf02c61341899
shachi agarwalla <shachiagarwalla@gmail.com>
2015-04-26 23:43:00 -0500

    Search and Review tests
    Added basic Search and Review tests

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -2,6 +2,8 @@ require "rails_helper"
 
 RSpec.feature "Search: ", :type => :feature do
   before :each do
+    load Rails.root + "db/skill_seeds.rb"
+    sleep (3)
     User.create(:email => "user@example.com", :password => "password", :city => "Madison", :zip_code => "53726", :state => "WI")
     visit "/users/sign_in"
     within("#new_user") do
@@ -15,8 +17,8 @@ RSpec.feature "Search: ", :type => :feature do
   scenario "visit search page" do
     expect(page).to have_selector(:link_or_button, 'Search')
     expect(page).to have_content "Skill"
-    # Test checkbox functionality here
-    # expect(page).to have_content "City State Radius"
+    expect(page).to have_content "Around me"
+    expect(page).to have_content "Radius (miles)"
   end
 
   scenario "visit profile page from search page" do
@@ -25,55 +27,214 @@ RSpec.feature "Search: ", :type => :feature do
     expect(page).to have_content "Please complete your profile before proceeding. UPDATE PROFILE"
   end
 
-=begin
-  scenario "search query shows no results (valid entries)" do
-    load Rails.root + "db/seeds.rb"
-    click_link("Search")
-    fill_in "City", :with => "Madison"
-    fill_in "State", :with => "WI"
-    fill_in "Radius", :with => "1"
-    select "Fishing", :from => "skill_id"
-    click_button "Search"
-    expect(page).to have_content "No results"
-    expect(page).to have_selector(:link_or_button, 'Search')
-    expect(page).to have_content "Skill"
-    expect(page).to have_content "City State Radius"
-    click_link 'About'
-    expect(page).to have_content 'About us page!'
-    click_link 'Help'
-    expect(page).to have_content 'Help!'
-    click_link 'How To'
-    expect(page).to have_content 'How to'
-    click_link 'Contact'
-    expect(page).to have_content 'Contact Us'
-    click_link 'Terms and Conditions'
-    expect(page).to have_content 'Terms and Conditions'
-    expect(page).to_not have_selector(:link_or_button, 'Login')
-    expect(page).to_not have_selector(:link_or_button, 'Sign Up')
-    expect(page).to have_selector(:link_or_button, 'Sign Out')
-    expect(page).to have_selector(:link_or_button, 'Search')
-    expect(page).to have_selector(:link_or_button, 'Profile')
+  scenario "checkbox checked" do
+    check('Around me')
+    should has_css?('optional-fields', :visible => false)
   end
-  
-  scenario "search query shows some result (valid entries)" do
-    load Rails.root + "db/seeds.rb"
-    click_link("Search")
-    fill_in "City", :with => "Madison"
-    fill_in "State", :with => "WI"
+ 
+  scenario "checkbox unchecked" do
+    uncheck('Around me')
+    should has_css?('optional-fields', :visible => true)
+  end
+
+  scenario "search query shows no results (Around me and valid entries)" do
     fill_in "Radius", :with => "10"
-    select "Cooking", :from => "skill_id"
     click_button "Search"
     expect(page).to have_content "Search Results"
-    expect(page).to have_content "Tutor"
-    expect(page).to have_content "Skills"
-    expect(page).to have_content "Rating"
-    page.should have_selector('table tr', :count => 13)
-    expect(page).to_not have_selector(:link_or_button, 'Login')
-    expect(page).to_not have_selector(:link_or_button, 'Sign Up')
-    expect(page).to have_selector(:link_or_button, 'Sign Out')
-    expect(page).to have_selector(:link_or_button, 'Search')
-    expect(page).to have_selector(:link_or_button, 'Profile')
+    page.should have_selector('table tr', :count => 1)
   end
-=end
-  
+
+  scenario "search for the skill that the logged in user is good at (Around me checked)" do
+    click_link("Profile")
+    fill_in "First name", :with => "dummy"
+    fill_in "Last name", :with => "example"
+    fill_in "City", :with => "Madison"
+    fill_in "State", :with => "Wisconsin"
+    fill_in "Zip code", :with => "53726"
+    fill_in "Date of birth", :with => "1989-11-23"
+    click_button("Update")
+    click_link("Add Skill")
+    fill_in "Description", :with => "Learned it"
+    select "4", :from =>  "Level"
+    select "Cooking", :from => "experience_skill_id"
+    fill_in "Start date", :with => "1999-11-23"
+    click_button "Submit"
+    click_link("Search")
+    select "Cooking", :from => "skill_id"
+    fill_in "Radius", :with => "10"
+    click_button "Search"
+    expect(page).to have_content "Search Results"
+    page.should have_selector('table tr', :count => 1)
+  end
+
+  scenario "search for the skill that the logged in user is good at (Around me unchecked)" do
+    click_link("Profile")
+    fill_in "First name", :with => "dummy"
+    fill_in "Last name", :with => "example"
+    fill_in "State", :with => "Wisconsin"
+    fill_in "Date of birth", :with => "1989-11-23"
+    click_button("Update")
+    click_link("Add Skill")
+    fill_in "Description", :with => "Learned it"
+    select "4", :from =>  "Level"
+    select "Cooking", :from => "experience_skill_id"
+    fill_in "Start date", :with => "1999-11-23"
+    click_button "Submit"
+    click_link("Search")
+    uncheck("Around me")
+    select("Wisconsin", :from => "state", visible: false)
+    fill_in("City", :with => "Madison", visible: false)
+    fill_in "Radius", :with => "10"
+    click_button "Search"
+    expect(page).to have_content "Search Results"
+    page.should have_selector('table tr', :count => 1)
+  end
+
+  scenario "search for the skill gives a result (Around me checked)" do
+    click_link("Profile")
+    fill_in "First name", :with => "dummy"
+    fill_in "Last name", :with => "example"
+    fill_in "City", :with => "Madison"
+    fill_in "State", :with => "Wisconsin"
+    fill_in "Zip code", :with => "53726"
+    fill_in "Date of birth", :with => "1989-11-23"
+    click_button("Update")
+    click_link("Add Skill")
+    fill_in "Description", :with => "Learned it"
+    select "4", :from =>  "Level"
+    select "Camping", :from => "experience_skill_id"
+    fill_in "Start date", :with => "1999-11-23"
+    click_button "Submit"
+    click_link("Sign Out")
+    User.create(:email => "user_new@example.com", :password => "password", :city => "Madison", :zip_code => "53701", :state => "WI")
+    visit "/users/sign_in"
+    within("#new_user") do
+      fill_in "Email", :with => "user_new@example.com"
+      fill_in "Password", :with => "password"
+    end
+    click_button "Log in"
+    click_link("Search")
+    select "Camping", :from => "skill_id"
+    fill_in "Radius", :with => "10"
+    click_button "Search"
+    expect(page).to have_content "Search Results"
+    page.should have_selector('table tr', :count => 2)
+  end
+
+  scenario "search for the skill gives a result (Around me unchecked)" do
+    click_link("Profile")
+    fill_in "First name", :with => "dummy"
+    fill_in "Last name", :with => "example"
+    fill_in "City", :with => "Madison"
+    fill_in "State", :with => "Wisconsin"
+    fill_in "Zip code", :with => "53726"
+    fill_in "Date of birth", :with => "1989-11-23"
+    click_button("Update")
+    click_link("Add Skill")
+    fill_in "Description", :with => "Learned it"
+    select "4", :from =>  "Level"
+    select "Camping", :from => "experience_skill_id"
+    fill_in "Start date", :with => "1999-11-23"
+    click_button "Submit"
+    click_link("Sign Out")
+    User.create(:email => "user_new@example.com", :password => "password", :city => "Madison", :zip_code => "53701", :state => "WI")
+    visit "/users/sign_in"
+    within("#new_user") do
+      fill_in "Email", :with => "user_new@example.com"
+      fill_in "Password", :with => "password"
+    end
+    click_button "Log in"
+    click_link("Search")
+    select "Camping", :from => "skill_id"
+    fill_in "Radius", :with => "10"
+    click_button "Search"
+    expect(page).to have_content "Search Results"
+    page.should have_selector('table tr', :count => 2)
+  end
+
+  scenario "search for the skill gives a result (Around me checked and user having same skill)" do
+    click_link("Profile")
+    fill_in "First name", :with => "dummy"
+    fill_in "Last name", :with => "example"
+    fill_in "City", :with => "Madison"
+    fill_in "State", :with => "Wisconsin"
+    fill_in "Zip code", :with => "53726"
+    fill_in "Date of birth", :with => "1989-11-23"
+    click_button("Update")
+    click_link("Add Skill")
+    fill_in "Description", :with => "Learned it"
+    select "4", :from =>  "Level"
+    select "Camping", :from => "experience_skill_id"
+    fill_in "Start date", :with => "1999-11-23"
+    click_button "Submit"
+    click_link("Sign Out")
+    User.create(:email => "user_new@example.com", :password => "password", :city => "Madison", :zip_code => "53701", :state => "WI")
+    visit "/users/sign_in"
+    within("#new_user") do
+      fill_in "Email", :with => "user_new@example.com"
+      fill_in "Password", :with => "password"
+    end
+    click_button "Log in"
+    click_link("Profile")
+    fill_in "First name", :with => "dummy_new"
+    fill_in "Last name", :with => "example"
+    fill_in "State", :with => "Wisconsin"
+    fill_in "Date of birth", :with => "1989-11-23"
+    click_button("Update")
+    click_link("Add Skill")
+    fill_in "Description", :with => "Learned it"
+    select "5", :from =>  "Level"
+    select "Camping", :from => "experience_skill_id"
+    fill_in "Start date", :with => "1999-11-23"
+    click_button "Submit"
+    click_link("Search")
+    select "Camping", :from => "skill_id"
+    fill_in "Radius", :with => "10"
+    click_button "Search"
+    expect(page).to have_content "Search Results"
+    page.should have_selector('table tr', :count => 2)
+  end
+
+  scenario "search for the skill gives a result (Around me unchecked and user having the same skill)" do
+    click_link("Profile")
+    fill_in "First name", :with => "dummy"
+    fill_in "Last name", :with => "example"
+    fill_in "City", :with => "Madison"
+    fill_in "State", :with => "Wisconsin"
+    fill_in "Zip code", :with => "53726"
+    fill_in "Date of birth", :with => "1989-11-23"
+    click_button("Update")
+    click_link("Add Skill")
+    fill_in "Description", :with => "Learned it"
+    select "4", :from =>  "Level"
+    select "Camping", :from => "experience_skill_id"
+    fill_in "Start date", :with => "1999-11-23"
+    click_button "Submit"
+    click_link("Sign Out")
+    User.create(:email => "user_new@example.com", :password => "password", :city => "Madison", :zip_code => "53701", :state => "WI")
+    visit "/users/sign_in"
+    within("#new_user") do
+      fill_in "Email", :with => "user_new@example.com"
+      fill_in "Password", :with => "password"
+    end
+    click_button "Log in"
+    click_link("Profile")
+    fill_in "First name", :with => "dummy_new"
+    fill_in "Last name", :with => "example"
+    fill_in "State", :with => "Wisconsin"
+    fill_in "Date of birth", :with => "1989-11-23"
+    click_button("Update")
+    click_link("Add Skill")
+    fill_in "Description", :with => "Learned it"
+    select "5", :from =>  "Level"
+    select "Camping", :from => "experience_skill_id"
+    fill_in "Start date", :with => "1999-11-23"
+    click_button "Submit"
+    click_link("Search")
+    select "Camping", :from => "skill_id"
+    fill_in "Radius", :with => "10"
+    click_button "Search"
+    expect(page).to have_content "Search Results"
+    page.should have_selector('table tr', :count => 2)
+  end
 end
