406e403253065a85b559e6a1519413db8528d8ae
Anthony To <to.anthony93@gmail.com>
2015-04-27 20:50:51 -0500

    Add verify_complete_profile action to application controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,15 +13,6 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "zapskills_platypus.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   
-  def complete_profile?
-    self.first_name != nil &&
-    self.last_name != nil &&
-    self.city != nil &&
-    self.state != nil &&
-    self.zip_code != nil &&
-    self.date_of_birth != nil
-  end
-  
   def location
     return "#{self.city}, #{self.state}, #{self.zip_code}"
   end
