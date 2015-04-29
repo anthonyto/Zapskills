279c1751bafb5dd59d429d7ae7c29f6b23cc82a1
shachi agarwalla <shachiagarwalla@gmail.com>
2015-04-28 20:41:03 -0500

    Post Integration Tests #2
    1. Footer_spec
    2. header_buttons_signedin_spec
    3. header_buttons_signedout_spec

--- spec/features/footer_spec.rb
+++ spec/features/footer_spec.rb
@@ -9,7 +9,7 @@ RSpec.feature "the footer to homepage process: ", :type => :feature do
     expect(page).to have_content 'Welcome to ZapSkills'
     expect(page).to have_content 'What is ZapSkills'
     expect(page).to have_selector(:link_or_button, 'Login')
-    expect(page).to have_selector(:link_or_button, 'Sign Up')
+    expect(page).to have_selector(:link_or_button, 'Get Started Today!')
     expect(page).to have_selector(:link_or_button, 'Search')
     expect(page).to_not have_selector(:link_or_button, 'Sign Out')
     expect(page).to_not have_selector(:link_or_button, 'Profile')
