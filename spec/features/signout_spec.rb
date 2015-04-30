427cad329d64d568b82805a3f6c5130b184f6bf2
shachi agarwalla <shachiagarwalla@gmail.com>
2015-04-30 16:12:38 -0500

    Signout test
    from search results page and add review

--- spec/features/signout_spec.rb
+++ spec/features/signout_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.feature "Signout process: ", :type => :feature do
   before :each do
+    sleep(1)
     User.create(:email => "user@example.com", :password => "password")
   end
 
@@ -95,4 +96,108 @@ RSpec.feature "Signout process: ", :type => :feature do
     click_link("Sign Out")
     expect(page).to have_content "Signed out successfully"
   end
+
+  scenario "search from search results" do
+    load Rails.root + "db/skill_seeds.rb"
+    visit "/users/sign_in"
+    within("#new_user") do
+      fill_in "Email", :with => "user@example.com"
+      fill_in "Password", :with => "password"
+    end
+    click_button "Log in"
+    click_link("Search")
+    click_link("Profile")
+    fill_in "First name", :with => "dummy"
+    fill_in "Last name", :with => "example"
+    select "Wisconsin", :from => "user_state"
+    fill_in "City", :with => "Madison"
+    fill_in "Zip code", :with => "53726"
+    fill_in "Date of birth", :with => "1989-11-23"
+    click_button("Update")
+    click_link("Add Skill")
+    fill_in "Description", :with => "Learned it"
+    select "Expert", :from =>  "Level"
+    select "Camping", :from => "experience_skill_id"
+    fill_in "Start date", :with => "1999-11-23"
+    click_button "Submit"
+    click_link("Sign Out")
+    User.create(:email => "user_second@example.com", :password => "password", :city => "Madison", :zip_code => "53701", :state => "WI")
+    visit "/users/sign_in"
+    within("#new_user") do
+      fill_in "Email", :with => "user_second@example.com"
+      fill_in "Password", :with => "password"
+    end
+    click_button "Log in"
+    click_link("Profile")
+    fill_in "First name", :with => "user_second"
+    fill_in "Last name", :with => "example"
+    select "Wisconsin", :from => "user_state"
+    fill_in "Date of birth", :with => "1989-11-23"
+    click_button("Update")
+    click_link("Search")
+    select "Camping", :from => "skill_id"
+    fill_in "Radius", :with => "10"
+    click_button "Search"
+    expect(page).to have_content "Search Results"
+    page.should have_selector('table tr', :count => 2)
+    click_link("Sign Out")
+    expect(page).to have_content "Signed out successfully"
+  end
+
+  scenario "from add review page" do
+    load Rails.root + "db/skill_seeds.rb"
+    visit "/users/sign_in"
+    within("#new_user") do
+      fill_in "Email", :with => "user@example.com"
+      fill_in "Password", :with => "password"
+    end
+    click_button "Log in"
+    click_link("Search")
+    click_link("Profile")
+    fill_in "First name", :with => "dummy"
+    fill_in "Last name", :with => "example"
+    select "Wisconsin", :from => "user_state"
+    fill_in "City", :with => "Madison"
+    fill_in "Zip code", :with => "53726"
+    fill_in "Date of birth", :with => "1989-11-23"
+    click_button("Update")
+    click_link("Add Skill")
+    fill_in "Description", :with => "Learned it"
+    select "Expert", :from =>  "Level"
+    select "Camping", :from => "experience_skill_id"
+    fill_in "Start date", :with => "1999-11-23"
+    click_button "Submit"
+    click_link("Sign Out")
+    User.create(:email => "user_second@example.com", :password => "password", :city => "Madison", :zip_code => "53701", :state => "WI")
+    visit "/users/sign_in"
+    within("#new_user") do
+      fill_in "Email", :with => "user_second@example.com"
+      fill_in "Password", :with => "password"
+    end
+    click_button "Log in"
+    click_link("Profile")
+    fill_in "First name", :with => "user_second"
+    fill_in "Last name", :with => "example"
+    select "Wisconsin", :from => "user_state"
+    fill_in "Date of birth", :with => "1989-11-23"
+    click_button("Update")
+    click_link("Add Skill")
+    fill_in "Description", :with => "Learned it"
+    select "Guru", :from =>  "Level"
+    select "Camping", :from => "experience_skill_id"
+    fill_in "Start date", :with => "1999-11-23"
+    click_button "Submit"
+    click_link("Search")
+    select "Camping", :from => "skill_id"
+    fill_in "Radius", :with => "10"
+    click_button "Search"
+    expect(page).to have_content "Search Results"
+    page.should have_selector('table tr', :count => 2)
+    find(:xpath, "//tr[td[contains(.,'Camping')]]/td/a", :text => 'dummy').click
+    click_link("Add Review")
+    expect(page).to have_content "Write A Review"
+    click_link("Sign Out")
+    expect(page).to have_content "Signed out successfully"
+  end
+
 end
