9646873664c7747177589ada9783520471b54b5c
Anthony To <to.anthony93@gmail.com>
2015-05-03 17:36:12 -0500

    Redirect to profile after sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   root 'welcome#main'
   
   get '/results', to: 'welcome#results', as: 'results'
