34725a77d30581565b2d4b484c4fa94605226c7d
shachi agarwalla <shachiagarwalla@gmail.com>
2015-05-04 11:41:35 -0500

    User controller tests update
    removed a bunch of unwanted controller tests and Date of births

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -63,7 +63,7 @@ RSpec.describe UsersController, :type => :controller do
       delete :destroy, id:@user.id
       flash[:notice].should_not be_nil
       flash[:notice].should eq("User was successfully destroyed.")
-      response.should redirect_to(users_url)
+      response.should redirect_to(user_path(assigns(:user)))
     end
     it "responds unsuccessfully" do
       sign_in nil
@@ -86,65 +86,6 @@ RSpec.describe UsersController, :type => :controller do
       sign_in nil
       @user = FactoryGirl.create :user
       get :show, id:@user.id
-      response.should_not be_success
-    end
-  end
-
-  describe "GET index" do
-    it "responds successfully with an HTTP 200 status code" do
-      sign_in
-      get :index
-      flash[:notice].should be_nil
-      response.should be_success
-      response.should have_http_status(200)
-      response.should render_template("index")
-    end
-    it "responds successfully with and user created" do
-      sign_in
-      @user = FactoryGirl.attributes_for :user
-      post :create, user:@user
-      get :index
-      assigns(:users).should_not be_empty
-      response.should be_success
-      response.should render_template("index")
-    end
-    it "responds unsuccessfully" do
-      sign_in nil
-      get :index
-      response.should_not be_success
-    end
-  end
-
-  describe "GET new" do
-    it "responds successfully" do
-      sign_in
-      get :new
-      flash[:notice].should be_nil
-      response.should be_success
-      response.should have_http_status(200)
-      response.should render_template("new")
-    end
-    it "responds unsuccessfully" do
-      sign_in nil
-      get :new
-      response.should_not be_success
-    end
-  end
-
-  describe "POST create" do
-    it "responds successfully and does not render new" do
-      sign_in
-      @user = FactoryGirl.attributes_for :user
-      post :create, user:@user
-      flash[:notice].should_not be_nil
-      flash[:notice].should eq("User was successfully created.")
-      response.should redirect_to(user_path(assigns(:user)))
-      response.should_not render_template("new")
-    end
-    it "responds unsuccessfully because not signed-in" do
-      sign_in nil
-      @user = FactoryGirl.attributes_for :user
-      post :create, user:@user
       response.should_not be_success
     end
   end
--- spec/factories.rb
+++ spec/factories.rb
@@ -11,8 +11,7 @@ FactoryGirl.define do
   end
   factory :experience do
     description "abcdfef"
-    start_date "1989-11-23"
-    level "4"
+    level "Advanced"
     skill
   end
   factory :user do |u|
@@ -22,9 +21,8 @@ FactoryGirl.define do
     u.password "foobar123"
     u.password_confirmation "foobar123"
     u.city "Madison"
-    u.state "WI"
+    u.state "Wisconsin"
     u.zip_code "53726"
-    u.date_of_birth "23/11/1989"
   end
 
 end
