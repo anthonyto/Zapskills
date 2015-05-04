5d7da1231084d8b41f25b2b1a04dbbf6387a01f6
shachi agarwalla <shachiagarwalla@gmail.com>
2015-05-04 11:05:03 -0500

    changes seeds
    removed date of birth

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,18 +23,18 @@ skills.each do |name|
 end
 
 users = [
-  ["Anthony", "To", "Madison", "WI", "53703", "01/10/1993", "password"],
-  ["Alex", "Faber", "Madison", "WI", "53703", "01/10/1993", "password"],
-  ["John", "Doe", "Madison", "WI", "53703", "01/10/1993", "password"],
-  ["Eric", "Miller", "Madison", "WI", "53703", "01/10/1993", "password"],
-  ["Jeanie", "To", "Madison", "WI", "53703", "01/10/1993", "password"],
-  ["Kyle", "Mchesney", "Madison", "WI", "53703", "01/10/1993", "password"],
-  ["Milad", "Torabi", "Madison", "WI", "53703", "01/10/1993", "password"],
-  ["Jasmine", "Kim", "Madison", "WI", "53703", "01/10/1993", "password"],
-  ["Cassie", "Kosky", "Madison", "WI", "53703", "01/10/1993", "password"],
-  ["Rachel", "Bourland", "Madison", "WI", "53703", "01/10/1993", "password"],
-  ["Ethan", "Ward", "Madison", "WI", "53703", "01/10/1993", "password"],
-  ["Rob", "Shepard", "Madison", "WI", "53703", "01/10/1993", "password"]
+  ["Anthony", "To", "Madison", "WI", "53703", "password"],
+  ["Alex", "Faber", "Madison", "WI", "53703", "password"],
+  ["John", "Doe", "Madison", "WI", "53703", "password"],
+  ["Eric", "Miller", "Madison", "WI", "53703", "password"],
+  ["Jeanie", "To", "Madison", "WI", "53703", "password"],
+  ["Kyle", "Mchesney", "Madison", "WI", "53703", "password"],
+  ["Milad", "Torabi", "Madison", "WI", "53703", "password"],
+  ["Jasmine", "Kim", "Madison", "WI", "53703", "password"],
+  ["Cassie", "Kosky", "Madison", "WI", "53703", "password"],
+  ["Rachel", "Bourland", "Madison", "WI", "53703", "password"],
+  ["Ethan", "Ward", "Madison", "WI", "53703", "password"],
+  ["Rob", "Shepard", "Madison", "WI", "53703", "password"]
 ]
 
 email_num = 0
@@ -42,14 +42,13 @@ email_num = 0
 skill = Skill.find_by(name: "Cooking")
 
 
-users.each do |first_name, last_name, city, state, zip_code, date_of_birth, password|
+users.each do |first_name, last_name, city, state, zip_code, password|
   user = User.create( first_name: first_name, 
                 last_name: last_name, 
                 email: "foo#{email_num}@bar.com", 
                 city: city, 
                 state: state, 
                 zip_code: zip_code, 
-                date_of_birth: date_of_birth, 
                 password: password)
   # user.experiences.create(description: "I stink at this", level: 1, skill_id: 1)
   # user.experiences.create(description: "I'm pretty good at this", level: 1, skill_id: rand(1..8))
