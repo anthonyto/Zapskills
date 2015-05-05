4a8d2fab40084df93f994ccb70c0a3b2604b1e38
shachi agarwalla <shachiagarwalla@gmail.com>
2015-05-05 12:26:49 -0500

    Signet tests
    Removed flash notices

--- spec/features/signout_spec.rb
+++ spec/features/signout_spec.rb
@@ -14,7 +14,7 @@ RSpec.feature "Signout process: ", :type => :feature do
     end
     click_button "Log in"
     click_link("Sign Out")
-    expect(page).to have_content "Signed out successfully"
+    expect(page).to have_selector(:link_or_button, 'Get Started Today!')
   end
 
   scenario "Without signing in redirects to signin page" do
@@ -30,9 +30,8 @@ RSpec.feature "Signout process: ", :type => :feature do
     end
     click_button "Log in"
     click_link("Profile")
-    expect(page).to have_content "You need to sign in or sign up before continuing"
     click_link("Sign Out")
-    expect(page).to have_content "Signed out successfully"
+    expect(page).to have_selector(:link_or_button, 'Get Started Today!')
   end
 
   scenario "sign out from edit profile" do
@@ -52,7 +51,7 @@ RSpec.feature "Signout process: ", :type => :feature do
     click_link("Edit")
     expect(page).to have_content "Update Profile"
     click_link("Sign Out")
-    expect(page).to have_content "Signed out successfully"
+    expect(page).to have_selector(:link_or_button, 'Get Started Today!')
   end
 
   scenario "add skills page" do
@@ -72,7 +71,7 @@ RSpec.feature "Signout process: ", :type => :feature do
     click_link("Add Skill")
     expect(page).to have_content "New Skill"
     click_link("Sign Out")
-    expect(page).to have_content "Signed out successfully"
+    expect(page).to have_selector(:link_or_button, 'Get Started Today!')
   end
 
   scenario "sign out from search" do
@@ -92,7 +91,7 @@ RSpec.feature "Signout process: ", :type => :feature do
     click_link("Search")
     expect(page).to have_content "Around me"
     click_link("Sign Out")
-    expect(page).to have_content "Signed out successfully"
+    expect(page).to have_selector(:link_or_button, 'Get Started Today!')
   end
 
   scenario "search from search results" do
@@ -115,7 +114,6 @@ RSpec.feature "Signout process: ", :type => :feature do
     fill_in "Description", :with => "Learned it"
     select "Expert", :from =>  "Level"
     select "Camping", :from => "experience_skill_id"
-    fill_in "Start date", :with => "1999-11-23"
     click_button "Submit"
     click_link("Sign Out")
     User.create(:email => "user_second@example.com", :password => "password", :city => "Madison", :zip_code => "53701", :state => "WI")
@@ -137,18 +135,18 @@ RSpec.feature "Signout process: ", :type => :feature do
     expect(page).to have_content "Search Results"
     page.should have_selector('table tr', :count => 2)
     click_link("Sign Out")
-    expect(page).to have_content "Signed out successfully"
+    expect(page).to have_selector(:link_or_button, 'Get Started Today!')
   end
 
   scenario "from add review page" do
     load Rails.root + "db/skill_seeds.rb"
+    User.create(:email => "user_second@example.com", :password => "password")
     visit "/users/sign_in"
     within("#new_user") do
       fill_in "Email", :with => "user@example.com"
       fill_in "Password", :with => "password"
     end
     click_button "Log in"
-    #click_link("Search")
     click_link("Profile")
     fill_in "First name", :with => "dummy"
     fill_in "Last name", :with => "example"
@@ -160,26 +158,25 @@ RSpec.feature "Signout process: ", :type => :feature do
     fill_in "Description", :with => "Learned it"
     select "Expert", :from =>  "Level"
     select "Camping", :from => "experience_skill_id"
-    fill_in "Start date", :with => "1999-11-23"
     click_button "Submit"
     click_link("Sign Out")
-    User.create(:email => "user_second@example.com", :password => "password", :city => "Madison", :zip_code => "53701", :state => "WI")
     visit "/users/sign_in"
     within("#new_user") do
       fill_in "Email", :with => "user_second@example.com"
       fill_in "Password", :with => "password"
     end
     click_button "Log in"
-    #click_link("Profile")
+    click_link("Profile")
     fill_in "First name", :with => "user_second"
     fill_in "Last name", :with => "example"
     select "Wisconsin", :from => "user_state"
+    fill_in "City", :with => "Madison"
+    fill_in "Zip code", :with => "53726"
     click_button("Update")
     click_link("Add Skill")
     fill_in "Description", :with => "Learned it"
     select "Guru", :from =>  "Level"
     select "Camping", :from => "experience_skill_id"
-    fill_in "Start date", :with => "1999-11-23"
     click_button "Submit"
     click_link("Search")
     select "Camping", :from => "skill_id"
@@ -191,7 +188,7 @@ RSpec.feature "Signout process: ", :type => :feature do
     click_link("Add Review")
     expect(page).to have_content "Write A Review"
     click_link("Sign Out")
-    expect(page).to have_content "Signed out successfully"
+    expect(page).to have_selector(:link_or_button, 'Get Started Today!')
   end
 
 end
